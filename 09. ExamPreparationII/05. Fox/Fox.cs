52f1b8598cfdac7c2cc914d76ed63eabc91e6f49
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2017-10-24 09:51:03 +0300

    Upload: Second Exam Preparation.

---
+++ 09. ExamPreparationII/05. Fox/Fox.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _05.Fox
+{
+    public class Fox
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int width = 2 * n + 3;
+
+            int star = 1;
+            int dash = width - 2 - 2 * star;
+            int eye = n / 2;
+            int midEye = n;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("{0}\\{1}/{0}", new string('*', star), new string('-', dash));
+                star++;
+                dash -= 2;
+            }
+            for (int i = 1; i <= n / 3; i++)
+            {
+                Console.WriteLine("|{0}\\{1}/{0}|", new string('*', eye), new string('*', midEye));
+                eye++;
+                midEye -= 2;
+            }
+            dash += 2;
+            star = width - 2 * dash - 2;
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("{0}\\{1}/{0}", new string('-', dash), new string('*', star));
+                dash++;
+                star -= 2;
+            }
+        }
+    }
+}
---
+++ 09. ExamPreparationII/06. Battles/Battles.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _06.Battles
+{
+    public class Battles
+    {
+        public static void Main()
+        {
+            int pokemon1 = int.Parse(Console.ReadLine());
+            int pokemon2 = int.Parse(Console.ReadLine());
+            int maxBattle = int.Parse(Console.ReadLine());
+            int battle = 0;
+
+            for (int i = 1; i <= pokemon1; i++)
+            {
+                for (int j = 1; j <= pokemon2; j++)
+                {
+                    if (battle >= maxBattle)
+                    {
+                        break;
+                    }
+                    Console.Write("({0} <-> {1}) ", i, j);
+                    battle++;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
