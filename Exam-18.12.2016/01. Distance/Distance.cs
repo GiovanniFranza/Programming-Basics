f0c5de5d99dce27dcf096ba91a41038a0c5de64b
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2017-10-26 08:43:37 +0300

    Upload: All tasks from Exam - 18.12.2016.

---
+++ Exam-18.12.2016/01. Distance/Distance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01.Distance
+{
+    public class Distance
+    {
+        public static void Main()
+        {
+            double kph = int.Parse(Console.ReadLine());
+            double firstTime = int.Parse(Console.ReadLine());
+            double secondTime = int.Parse(Console.ReadLine());
+            double thirdTime = int.Parse(Console.ReadLine());
+
+            double firstLenght = kph * (firstTime / 60);
+            double secondSpeed = kph + (kph * 0.10);
+            double secondLenght = secondSpeed * (secondTime / 60);
+            double thirdSpeed = secondSpeed - (secondSpeed * 0.05);
+            double thirdLenght = thirdSpeed * (thirdTime / 60);
+            double total = firstLenght + secondLenght + thirdLenght;
+
+            Console.WriteLine("{0:F2}", total);
+        }
+    }
+}
