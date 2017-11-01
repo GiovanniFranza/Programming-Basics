15473254f26ef5227abd0797fd838460be17863f
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2017-11-01 17:46:26 +0200

    Refactoring all names and variables.

---
+++ 01. FirstStepsInCoding/05. RectangleArea/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var a = decimal.Parse(Console.ReadLine());
+        var b = decimal.Parse(Console.ReadLine());
+
+        var area = a * b;
+
+        Console.WriteLine(area);
+    }
+}
+
