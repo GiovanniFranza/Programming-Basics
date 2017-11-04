85a0347ebd435d0f6a034dfb749ddedfa696e2c9
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2017-11-04 10:44:10 +0200

    Update: Refactoring all classes' names.

---
+++ 08. ExamPreparationI/03. BikeRace/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        int young = int.Parse(Console.ReadLine());
+        int old = int.Parse(Console.ReadLine());
+        string road = Console.ReadLine();
+
+        int people = young + old;
+
+        if (road == "trail")
+        {
+            double taxeYoung = young * 5.50;
+            double taxeOld = old * 7;
+            double expenses = (taxeYoung + taxeOld) * 0.05;
+            double total = taxeYoung + taxeOld - expenses;
+            Console.WriteLine("{0:F2}", total);
+        }
+        else if (road == "cross-country")
+        {
+            if (people >= 50)
+            {
+                double taxeYoung = young * (8 - (8 * 0.25));
+                double taxeOld = old * (9.50 - (9.50 * 0.25));
+                double expenses = (taxeYoung + taxeOld) * 0.05;
+                double total = taxeYoung + taxeOld - expenses;
+                Console.WriteLine("{0:F2}", total);
+            }
+            else
+            {
+                double taxeYoung = young * 8;
+                double taxeOld = old * 9.50;
+                double expenses = (taxeYoung + taxeOld) * 0.05;
+                double total = taxeYoung + taxeOld - expenses;
+                Console.WriteLine("{0:F2}", total);
+            }
+        }
+        else if (road == "downhill")
+        {
+            double taxeYoung = young * 12.25;
+            double taxeOld = old * 13.75;
+            double expenses = (taxeYoung + taxeOld) * 0.05;
+            double total = taxeYoung + taxeOld - expenses;
+            Console.WriteLine("{0:F2}", total);
+        }
+        else if (road == "road")
+        {
+            double taxeYoung = young * 20;
+            double taxeOld = old * 21.50;
+            double expenses = (taxeYoung + taxeOld) * 0.05;
+            double total = taxeYoung + taxeOld - expenses;
+            Console.WriteLine("{0:F2}", total);
+        }
+    }
+}
---
+++ 08. ExamPreparationI/06. MaxCombination/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        int start = int.Parse(Console.ReadLine());
+        int finish = int.Parse(Console.ReadLine());
+        int max = int.Parse(Console.ReadLine());
+        int combination = 0;
+
+        for (int i = start; i <= finish; i++)
+        {
+            for (int j = start; j <= finish; j++)
+            {
+                if (combination >= max)
+                {
+                    break;
+                }
+                Console.Write("<{0}-{1}>", i, j);
+                combination++;
+            }
+        }
+        Console.WriteLine();
+    }
+}
